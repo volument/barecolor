0782543a6d007552d564d34a611e9052ca15a06a
Tero Piirainen <tipiirai@gmail.com>
2021-12-08 10:27:35 +0200

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const colors = { black: 30, red: 31, green: 32, yellow: 33, blue: 34, magenta: 35, cyan: 36, white: 37, gray: 90 },
-  supported = /color|ansi|cygwin|linux/i.test(process.env.TERM)
-
+  supported = /_color|ansi|cygwin|linux/i.test(process.env.TERM)
 
 for (let name in colors ) {
   const color = colors[name]
