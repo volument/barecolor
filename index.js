95cbe3b21e455e2e1b1118ce40e7a4c616a4b66d
Michał Miszczyszyn <michal@mmiszy.pl>
2021-12-25 18:47:51 +0100

    Add support for xterm (MacOS terminal)
    Fixes #1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const colors = { black: 30, red: 31, green: 32, yellow: 33, blue: 34, magenta: 35, cyan: 36, white: 37, gray: 90 },
-  supported = /_color|ansi|cygwin|linux/i.test(process.env.TERM)
+  supported = /_color|ansi|cygwin|linux|xterm/i.test(process.env.TERM)
 
 for (let name in colors ) {
   const color = colors[name]
